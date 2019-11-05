eb5942c05e0d4082f05e06f9d22218d9a0e7030c
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-11-05 19:41:37 +0900

    신세경 추가됨

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,14 @@
 $(document).ready(()=>{
     let list = [
-        { name : "유경수" , account : "yoogomja" , url : "https://github.com/yoogomja" },
-        { name : "배성현" , account : "baeseongh" , url : "https://github.com/baeseongh" },
+        { nm : "유경수" , user_nm : "yoogomja" , url : "https://github.com/yoogomja" },
+        { nm : "배성현" , user_nm : "baeseongh" , url : "https://github.com/baeseongh" },
+        { nm : "신세경" , user_nm : "신세경" , url : "https://github.com/baeseongh" },
     ];
     list.map((item , idx) =>{
         let copied = $('.mockup').clone().removeClass('mockup');
-        copied.find('.item-name').text(item.name);
+        copied.find('.item-name').text(item.nm);
         copied.find('.item-link').attr('href' , item.url);
-        copied.find('.item-github-account').text("@" + item.account);
+        copied.find('.item-github-account').text("@" + item.user_nm);
         $('.list-items').append(copied);
     });
 });
