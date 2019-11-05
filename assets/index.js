0d10811f7ca76bba11dadebd131cf75bcdecec2f
Ksanbal <hyungyun96@gmail.com>
2019-11-05 20:01:43 +0900

    Add Ksanbal’s git acount

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ $(document).ready(()=>{
         { nm : "배성현" , user_nm : "baeseongh" , url : "https://github.com/baeseongh" },
         { nm : "신세경" , user_nm : "신세경" , url : "https://github.com/baeseongh" },
         { nm : "송다운" , user_nm : "haryoung" , url : "https://github.com/HaRyoung" },
+        { nm : "김현균" , user_nm : "Ksanbal" , url : "https://github.com/Ksanbal" },
     ];
     list.map((item , idx) =>{
         let copied = $('.mockup').clone().removeClass('mockup');
