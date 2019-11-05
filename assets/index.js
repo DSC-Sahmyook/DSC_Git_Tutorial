314596b75cadd0f9c6298980af21ac91f488065a
HaRyoung <qsss@naver.com>
2019-11-05 19:44:19 +0900

    Do you know do you

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,7 @@ $(document).ready(()=>{
     let list = [
         { name : "유경수" , account : "yoogomja" , url : "https://github.com/yoogomja" },
         { name : "배성현" , account : "baeseongh" , url : "https://github.com/baeseongh" },
+        { name : "송다운" , account : "haryoung" , url : "https://github.com/HaRyoung" },
     ];
     list.map((item , idx) =>{
         let copied = $('.mockup').clone().removeClass('mockup');
