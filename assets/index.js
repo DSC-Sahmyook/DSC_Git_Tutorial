c03a74a04ef0bb7396a0435b48a722d05b856b88
parkjoungwan <whddhs6645@gmail.com>
2019-11-05 19:59:14 +0900

    joungwan2

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,13 @@
 $(document).ready(()=>{
     let list = [
-<<<<<<< HEAD
         { name : "유경수" , account : "yoogomja" , url : "https://github.com/yoogomja" },
         { name : "배성현" , account : "baeseongh" , url : "https://github.com/baeseongh" },
         { name : "박종완" , account : "Parkjoungwan" , url : "https://github.com/Parkjoungwan" },
         { name : "송다운" , account : "haryoung" , url : "https://github.com/HaRyoung" },
-=======
         { nm : "유경수" , user_nm : "yoogomja" , url : "https://github.com/yoogomja" },
         { nm : "배성현" , user_nm : "baeseongh" , url : "https://github.com/baeseongh" },
         { nm : "신세경" , user_nm : "신세경" , url : "https://github.com/baeseongh" },
         { nm : "송다운" , user_nm : "haryoung" , url : "https://github.com/HaRyoung" },
->>>>>>> 8b88b72e877439f7c61a89601618a3e406d1c011
     ];
     list.map((item , idx) =>{
         let copied = $('.mockup').clone().removeClass('mockup');
