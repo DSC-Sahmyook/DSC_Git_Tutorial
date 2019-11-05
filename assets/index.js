27e57314b99f71d5f4e7ecdfa8c8c4d4201fdbc2
Parkjoungwan <whddhs6645@gmail.com>
2019-11-05 19:41:41 +0900

    박종완 추가됨

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,7 @@ $(document).ready(()=>{
     let list = [
         { name : "유경수" , account : "yoogomja" , url : "https://github.com/yoogomja" },
         { name : "배성현" , account : "baeseongh" , url : "https://github.com/baeseongh" },
+        { name : "박종완" , account : "Parkjoungwan" , url : "https://github.com/Parkjoungwan" },
     ];
     list.map((item , idx) =>{
         let copied = $('.mockup').clone().removeClass('mockup');
