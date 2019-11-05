9a6094c2ff3d8df2964c82e4ad5236b2e916a062
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-11-05 17:55:10 +0900

    first commit

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+    let list = [
+        { name : "유경수" , account : "yoogomja" , url : "https://github.com/yoogomja" },
+        { name : "배성현" , account : "baeseongh" , url : "https://github.com/baeseongh" },
+    ];
+    
+    list.map((item , idx) =>{
+        let copied = $('.mockup').clone().removeClass('mockup');
+        copied.find('.item-name').text(item.name);
+        copied.find('.item-link').attr('href' , item.url);
+        copied.find('.item-github-account').text("@" + item.account);
+        $('.list-items').append(copied);
+    });
+});
