6e8f36220e1676cbb8a0e3d00dc1738a9306f79b
Lee Bom <bome0214@naver.com>
2019-11-05 19:46:27 +0900

    leebom done

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,8 @@ $(document).ready(()=>{
     let list = [
         { name : "유경수" , account : "yoogomja" , url : "https://github.com/yoogomja" },
         { name : "배성현" , account : "baeseongh" , url : "https://github.com/baeseongh" },
+        { name : "이 봄" , account : "leebom0214" , url : "https://github.com/leebom0214" },
+
     ];
     list.map((item , idx) =>{
         let copied = $('.mockup').clone().removeClass('mockup');
